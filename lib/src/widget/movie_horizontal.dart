3a990370acb579aaf58afdf3062b356c9966b8f9
kervisvasquez1993 <devpsi@MacBook-Pro-de-Premiere.local>
2020-12-09 16:43:19 -0400

    agregamos Hero animation

--- lib/src/widget/movie_horizontal.dart
+++ lib/src/widget/movie_horizontal.dart
@@ -39,13 +39,16 @@ class MovieHorizontal extends StatelessWidget {
       margin: EdgeInsets.only(right: 10.0),
       child: Column(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: FadeInImage(
-              image: NetworkImage(pelicula.getPosterImg()),
-              placeholder: AssetImage('assets/img/no-image.jpg'),
-              fit: BoxFit.cover,
-              height: 110.0,
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage(pelicula.getPosterImg()),
+                placeholder: AssetImage('assets/img/no-image.jpg'),
+                fit: BoxFit.cover,
+                height: 110.0,
+              ),
             ),
           ),
           //SizedBox(height: 5.0),
