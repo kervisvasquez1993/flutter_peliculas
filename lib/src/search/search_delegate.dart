e75ffcb488c9623b11743141231179233c8d7fb5
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-12-14 11:01:38 -0400

    agregamos search delegation para buscar peliculas

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // las acciones de nuestro App
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    //  Iconos a la izquierda del appbar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // crea ,os resultado que vamos a mostrar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // sujerencias que aparencen cuando la persona escribe
+    throw UnimplementedError();
+  }
+}
